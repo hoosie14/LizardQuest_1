c206b8bce38e8d0c9d5cc2584358f43e5f30e477
EZB1 <Ethanzbestsb@gmail.com>
2025-07-22 19:09:08 -0700

    I added the main menu and options menu including the sprites for them.

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	public void GoToOptions ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+	}
+
+
+	public void QuitGame ()
+	{
+		Debug.Log("Quit!");	
+		Application.Quit();
+	}
+}
